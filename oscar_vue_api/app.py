f1db3100c457c47c0a770f49f3a81cec32000869
Yaisel Hurtado <hurta2yaisel@gmail.com>
2020-10-31 22:12:16 -0300

    Ajustes de links

--- oscar_vue_api/app.py
+++ oscar_vue_api/app.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from oscarapi.app import RESTApiApplication
+from oscarapi.apps import OscarAPIConfig as BaseOscarAPIConfig
 from oscar_vue_api.authtoken import obtain_auth_token
 from . import views
 
-class MyRESTApiApplication(RESTApiApplication):
 
+class OscarAPIConfig(BaseOscarAPIConfig):
     def get_urls(self):
         urls = [
             url(r'^user/login', obtain_auth_token),
@@ -15,9 +15,10 @@ class MyRESTApiApplication(RESTApiApplication):
             url(r'^cart/delete', views.DeleteBasketItemView.as_view()),
             url(r'^cart/totals', views.BasketTotalsView.as_view()),
             url(r'^cart/shipping-information', views.BasketTotalsView.as_view()),
-            #url(r'^products/index', views.ProductList.as_view(), name='product-list'),
+            # url(r'^products/index', views.ProductList.as_view(), name='product-list'),
             url(r'^catalog', views.ElasticView.as_view()),
         ]
         return urls + super(MyRESTApiApplication, self).get_urls()
 
-application = MyRESTApiApplication()
+
+application = OscarAPIConfig()
